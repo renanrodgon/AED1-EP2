8b6925761e6ac2d4bb5c144e7f97ff6e0ca94c31
Renan Rodrigues Gonçalves <renanrodgon@yahoo.com.br>
2016-11-15 18:26:12 -0200

    Projeto Criado
    O projeto foi criado e juntamente dele, os arquivos .c pertencentes ao
    Exercicio e os pdfs de especificações

---
+++ FilaDePrioridade.c
@@ -0,0 +1,57 @@
+#include "FilaDePrioridade.h"
+
+PFILA criarFila(int max){
+  PFILA res = (PFILA) malloc(sizeof(FILADEPRIORIDADE));
+  res->maxRegistros = max;
+  res->arranjo = (PONT*) malloc(sizeof(PONT)*max);
+  res->heap = (PONT*) malloc(sizeof(PONT)*max);
+  int i;
+  for (i=0;i<max;i++) {
+    res->arranjo[i] = NULL;
+    res->heap[i] = NULL;
+  }
+  res->elementosNoHeap = 0;
+  return res;
+}
+
+bool exibirLog(PFILA f){
+  printf("Log [elementos: %i]\n", f->elementosNoHeap);
+  PONT atual;
+  int i;
+  for (i=0;i<f->elementosNoHeap;i++){
+    atual = f->heap[i];
+    printf("[%i;%f;%i] ", atual->id, atual->prioridade, atual->posicao);
+  }
+  printf("\n\n");
+}
+
+int tamanho(PFILA f){
+  /* completar */
+  return 0;
+}
+
+bool inserirElemento(PFILA f, int id, float prioridade){
+  /* completar */
+  return false;
+}
+
+bool aumentarPrioridade(PFILA f, int id, float novaPrioridade){
+  /* completar */
+  return false;
+}
+
+bool reduzirPrioridade(PFILA f, int id, float novaPrioridade){
+  /* completar */
+  return false;
+}
+
+PONT removerElemento(PFILA f){
+  /* completar */
+  return NULL;
+}
+
+bool consultarPrioridade(PFILA f, int id, float* resposta){
+  /* completar */
+  return false;
+}
+
